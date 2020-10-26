ddf1e3db1501b173928f956c232e8d962ef9926a
tincogs <tomoday91@gmail.com>
2020-10-26 17:09:54 -0500

    commit tree 27a4c9d5610712f3e50fc7d1723c4c5b0852f321

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(people, reason){
+    let thankYouCards = []
+    for (let i = 0; i < people.length; i++) {
+        thankYouCards.push(`Thank you, ${people[i]}, for the wonderful ${reason} gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(num) {
+    while (num > 0) {
+       console.log(num);
+       num -= 1;
+    }
+    console.log(num)
+}
